d7eb36345a0db7ea6a5eb89e1210ad1ae59287f9
NTH931 <noahth931@gmail.com>
2026-03-01 18:14:35 +1300

    carver 2.0.0 commit

--- src/main/java/com/leathershorts/carver/CarverDataGenerator.java
+++ src/main/java/com/leathershorts/carver/CarverDataGenerator.java
@@ -7,15 +7,12 @@ import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.registry.RegistryWrapper;
 import org.jetbrains.annotations.NotNull;
 
-public class CarverDataGenerator implements DataGeneratorEntrypoint {
 import java.util.concurrent.CompletableFuture;
 
 final class CarverDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(@NotNull FabricDataGenerator fabricDataGenerator) {
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
-		pack.addProvider(CarverRecipeProvider::new);
-
         var blockTags = pack.addProvider(BlockTagGenerator::new);
         pack.addProvider((out, reg) -> new ItemTagGenerator(out, reg, blockTags));
         pack.addProvider(CarverRecipeProvider::new);
